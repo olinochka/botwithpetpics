d111c2fb24f9b096c30eab37c93ad179214e0e02
Alina <99502344+olinochka@users.noreply.github.com>
2023-06-15 15:38:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,35 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ReplyKeyboardMarkup
 import mysql.connector
 from PIL import Image
 import io
 import base64
+import tempfile
 
 token = "***" # this is where telegram token goes
 
-# function for the /start command
+# Function for the /start command
 def start(update, context):
     hellotext = "Привет! Я твой поставщик фотографий животных в одёжке :) Нажми кнопку, чтобы получить серотонин."
-    my_keyboard = ReplyKeyboardMarkup([['Хочу серотонина']], resize_keyboard = True)
-    update.message.reply_text(hellotext, reply_markup = my_keyboard)
+    my_keyboard = ReplyKeyboardMarkup([['Хочу серотонина']], resize_keyboard=True)
+    update.message.reply_text(hellotext, reply_markup=my_keyboard)
 
-# function for errors from the dispatcher
+# Function for errors from the dispatcher
 def error(update, context):
     errortext = "Кажется, произошла какая-то ошибка :( Наши выдры-программисты обязательно с ней разберутся"
     update.message.reply_text(errortext)
 
-# function for sending image
+# Function for sending image
 def serotonin(update, context):
-    # establishing mysql connection
+    # Establishing MySQL connection
     conn = mysql.connector.connect(
-    host="***",
-    port='3306',
-    user="***",
-    password="***",
-    database ="***"
+    	host="***",
+    	port='3306',
+    	user="***",
+    	password="***",
+    	database ="***"
     )
-    
+
     query = ("SELECT pic FROM pets ORDER BY RAND() LIMIT 1")
 
     cursor = conn.cursor(buffered=True)
@@ -40,22 +41,28 @@ def serotonin(update, context):
         return justbytes
 
     justbytes = convertTuple(tuple_record[0])
+
     base64_str = base64.b64encode(justbytes).decode('utf-8')
-    imgdata = base64.b64decode(str(base64_str))
+
+    imgdata = base64.b64decode(base64_str)
     petPic = Image.open(io.BytesIO(imgdata))
-    petPic.save('/home/olina/projects/forBot/test.jpeg')
-    updater.bot.send_photo(chat_id=update.message.chat_id, photo=open('/home/olina/projects/forBot/test.jpeg', 'rb'))
+
+    with tempfile.NamedTemporaryFile(suffix='.jpg') as temp_file:
+        petPic.save(temp_file.name)
+        temp_file.seek(0)  # Reset the file pointer to the beginning
+
+        # Send the image to the user
+        context.bot.send_photo(chat_id=update.message.chat_id, photo=temp_file)
 
     cursor.close()
     conn.close()
 
-# function for answering to everything that is unplanned
+# Function for answering to everything that is unplanned
 def unknown(update, context):
-	chat = update.effective_chat
-	context.bot.send_message(chat_id=chat.id, text="Пока я умею только делиться фотографиями, поболтать лучше с Алисой.")
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text="Пока я умею только делиться фотографиями, поболтать лучше с Алисой.")
 
-updater = None
-updater = Updater(token, use_context=True)
+updater = Updater(token)
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler("start", start))
